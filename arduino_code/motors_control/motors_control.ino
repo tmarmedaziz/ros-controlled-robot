d501ce6efea17e0eed027c7e9688673279d21405
tmarmedaziz <mohamedaziz.tmar@supcom.tn>
2024-10-11 23:01:41 +0100

    Add base control arduino code + current project architecture

---
+++ arduino_code/motors_control/motors_control.ino
@@ -0,0 +1,62 @@
+int FRONT_RIGHT_DIRECTION_1_PIN = 4;
+int FRONT_LEFT_DIRECTION_1_PIN = 7;
+int REAR_RIGHT_DIRECTION_1_PIN = 3;
+int REAR_LEFT_DIRECTION_1_PIN = 8;
+
+int FRONT_RIGHT_DIRECTION_2_PIN = 5;
+int FRONT_LEFT_DIRECTION_2_PIN = 6;
+int REAR_RIGHT_DIRECTION_2_PIN = 2;
+int REAR_LEFT_DIRECTION_2_PIN = 9;
+
+void controlMotorDirection(int in1, int in2, int direction);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(FRONT_RIGHT_DIRECTION_1_PIN, OUTPUT);
+  pinMode(FRONT_LEFT_DIRECTION_1_PIN, OUTPUT);
+  pinMode(REAR_RIGHT_DIRECTION_1_PIN, OUTPUT);
+  pinMode(REAR_LEFT_DIRECTION_1_PIN, OUTPUT);
+
+  pinMode(FRONT_RIGHT_DIRECTION_2_PIN, OUTPUT);
+  pinMode(FRONT_LEFT_DIRECTION_2_PIN, OUTPUT);
+  pinMode(REAR_RIGHT_DIRECTION_2_PIN, OUTPUT);
+  pinMode(REAR_LEFT_DIRECTION_2_PIN, OUTPUT);
+
+  controlMotorDirection(FRONT_RIGHT_DIRECTION_1_PIN, FRONT_RIGHT_DIRECTION_2_PIN, 0);
+  controlMotorDirection(FRONT_LEFT_DIRECTION_1_PIN, FRONT_LEFT_DIRECTION_2_PIN, 0);
+  controlMotorDirection(REAR_RIGHT_DIRECTION_1_PIN, REAR_RIGHT_DIRECTION_2_PIN, 0);
+  controlMotorDirection(REAR_LEFT_DIRECTION_1_PIN, REAR_LEFT_DIRECTION_2_PIN, 0);
+}
+
+void loop() {
+  // Turn on FRONT_RIGHT all motors
+  controlMotorDirection(FRONT_RIGHT_DIRECTION_1_PIN, FRONT_RIGHT_DIRECTION_2_PIN, 1);
+  controlMotorDirection(FRONT_LEFT_DIRECTION_1_PIN, FRONT_LEFT_DIRECTION_2_PIN, 1);
+  controlMotorDirection(REAR_RIGHT_DIRECTION_1_PIN, REAR_RIGHT_DIRECTION_2_PIN, 1);
+  controlMotorDirection(REAR_LEFT_DIRECTION_1_PIN, REAR_LEFT_DIRECTION_2_PIN, 1);
+  Serial.println("FRONT_RIGHT motor running");
+  delay(2000);  // Run for 2 seconds
+
+  // Turn off all motors
+  controlMotorDirection(FRONT_RIGHT_DIRECTION_1_PIN, FRONT_RIGHT_DIRECTION_2_PIN, 0);
+  controlMotorDirection(FRONT_LEFT_DIRECTION_1_PIN, FRONT_LEFT_DIRECTION_2_PIN, 0);
+  controlMotorDirection(REAR_RIGHT_DIRECTION_1_PIN, REAR_RIGHT_DIRECTION_2_PIN, 0);
+  controlMotorDirection(REAR_LEFT_DIRECTION_1_PIN, REAR_LEFT_DIRECTION_2_PIN, 0);
+  delay(500);  // wait for 500ms
+}
+
+void controlMotorDirection(int in1, int in2, int direction) {
+  // 0 to  stop motro, 1 for dir1 and -1 for dir2
+  if (direction == 0) {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+  } else if (direction == 1) {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  } else if (direction == -1) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  } else {
+    return;
+  }
+}
